a18a80eb9a2959a4df579561ffbae34e125483ed
Michael_Montero <mmontero2012@gmail.com>
2015-06-23 17:53:13 -0700

    gulp intact

--- Upnode/routes/index.js
+++ Upnode/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '../views/index.html') );
+  console.log('Welcome');
 });
+
+router.post('/upload', function(req, res){
+	
+});
+
+
 
 module.exports = router;
