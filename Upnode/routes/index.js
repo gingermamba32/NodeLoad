2f1b423cbce598c6cb5f606f784e76ff95455340
Michael_Montero <mmontero2012@gmail.com>
2015-06-25 18:10:41 -0700

    added crud model

--- Upnode/routes/index.js
+++ Upnode/routes/index.js
@@ -16,11 +16,48 @@ router.get('/helloworld', function(req,res,next){
 router.get('/userlist', function(req, res) {
     var db = req.db;
     var collection = db.get('usercollection');
-    collection.find({},{},function(err, docs){
-        res.render('userlist', {
+    collection.find({},function(err, docs){
+        res.render('userlist.jade', {
             "userlist" : docs
         });
     });
 });
+
+// get new user from the database
+router.get('/newuser', function(req,res){
+	res.render('newuser.jade', {title: 'Welcome new user'})
+})
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
+
+
 
 module.exports = router;
